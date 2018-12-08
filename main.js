2d5ba0b27c700426ee5c7f3929d277c286819556
PakithAria <panzoupepe@outlook.com>
2018-12-08 18:13:52 -0500

    modif debut de js

--- main.js
+++ main.js
@@ -9,15 +9,16 @@ var mainVue = function ()
         form: undefined,
         nameInput: undefined,
     
+        firstNameInput: undefined,
+
         submit: undefined,
     
         render: undefined,
+
         nameParagraphe: undefined,
 
-    };
+        firstNameParagraphe: undefined,
 
-    this.model ={
-        name: '',
     };
 
 };
@@ -29,20 +30,30 @@ mainVue.prototype.init=function()
 
     this.elements.nameInput=this.elements.form.querySelector('[data-model="name"]');
 
+    this.elements.firstNameInput=this.elements.form.querySelector('[data-model="firstname"]');
+
     this.elements.submit=this.elements.form.querySelector('[data-action="submit"]');
 
     this.elements.render=this.elements.main.querySelector('[data-render="'+this.vue+'"]');
 
     this.elements.nameParagraphe=this.elements.render.querySelector('[data-model="name"]');
 
+    this.elements.firstNameParagraphe=this.elements.render.querySelector('[data-model="firstname"]');
+
     this.registerAction();
+    
 
 };
 
 mainVue.prototype.registerAction=function(){
     var self=this;
     this.elements.submit.addEventListener('click',function(){
-        self.submit() });
+        self.submit();
+    });
+    
+    this.elements.firstNameInput.addEventListener('keydown',function(){
+            self.write(); 
+    });
 };
 
 mainVue.prototype.submit=function(){
@@ -50,5 +61,10 @@ mainVue.prototype.submit=function(){
     self.elements.nameParagraphe.innerHTML=self.elements.nameInput.value; 
 };
 
+
+mainVue.prototype.write=function(){
+    var self =this;
+    self.elements.firstNameParagraphe.innerHTML=self.elements.firstNameInput.value; 
+};
 var m=new mainVue();
 m.init();
